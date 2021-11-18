e3d491f409919e67c2f1dbd98a38934064c8f547
Meesum A. Qazalbash <62182585+MeesumAliQazalbash@users.noreply.github.com>
2021-11-18 13:06:20 +0500

    new

---
+++ screen.cpp
@@ -0,0 +1,9 @@
+#include <screen.h>
+
+void __screen__::update()
+{
+}
+
+__screen__::__screen__()
+{
+}
---
+++ screen.h
@@ -0,0 +1,12 @@
+#include <constant.h>
+#include <stack.h>
+
+class __screen__
+{
+private:
+    __stack__ screen[10];
+
+public:
+    void update();
+    __screen__();
+};
