af75726c26731e93736318fbf75f9f712718f48c
Ahmed Atif <65737137+ProValarous@users.noreply.github.com>
2021-11-18 13:04:57 +0500

    Block class ...
    note: note complete

---
+++ block.cpp
@@ -0,0 +1,42 @@
+#include "block.h"
+
+Block::Block(int randomNo)
+{
+    switch (randomNo)
+    {
+    case 1:
+        /* code */
+        break;
+    case 2:
+        /* code */
+        break;
+    case 3:
+        /* code */
+        break;
+    case 4:
+        /* code */
+        break;
+    case 5:
+        /* code */
+        break;
+    
+    default:
+        break;
+    }
+}
+
+
+void Block::draw()
+{
+
+}
+
+void Block::fall()
+{
+
+}
+
+void Block::status()
+{
+
+}
---
+++ block.h
@@ -0,0 +1,24 @@
+#include "matrix.h"
+
+
+class Block{
+    private:
+    int xCord;
+    int yCord;
+    
+
+
+
+    public:
+    Block(int randomNo);
+
+    void draw();
+
+    void fall();
+
+    //debugging functions
+    void status();
+
+
+
+};
