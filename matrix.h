6fba559e00dc287ec43c3686d75b14f68f566377
Meesum A. Qazalbash <62182585+MeesumAliQazalbash@users.noreply.github.com>
2021-11-18 11:49:16 +0500

    Update matrix.h

--- matrix.h
+++ matrix.h
@@ -1,12 +1,17 @@
 #include <stdio.h>
-#include "constant.h"
+
+#define ROWS 15
+#define COLS 15
 
 class __matrix__
 {
+    int rows;
+    int cols;
     bool matrix[ROWS][COLS];
 
 public:
-    void set(int row, int col, bool value);
-    bool get(int row, int col);
+    void set(int, int, bool);
+    bool get(int, int);
+    __matrix__ operator+(__matrix__);
     __matrix__();
 };
