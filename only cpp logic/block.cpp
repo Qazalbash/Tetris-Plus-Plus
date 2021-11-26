2d42403eddf2bdb077f4c968b599b03f8643ada5
Meesum A. Qazalbash <62182585+MeesumAliQazalbash@users.noreply.github.com>
2021-11-26 14:31:53 +0500

    block modified

--- only cpp logic/block.cpp
+++ only cpp logic/block.cpp
@@ -8,9 +8,9 @@ private:
 
     void nullBlock()
     {
-        for (int blockRowArrow = 0; blockRowArrow < 4; blockRowArrow++)
+        for (int blockRowArrow = 0; blockRowArrow < 5; blockRowArrow++)
         {
-            for (int blockColArrow = 0; blockColArrow < 4; blockColArrow++)
+            for (int blockColArrow = 0; blockColArrow < 5; blockColArrow++)
             {
                 blk[blockRowArrow][blockColArrow] = 0;
             }
@@ -93,18 +93,18 @@ public:
 
     void operator~()
     {
-        int temporaryBlock[4][4];
-        for (int blockRowArrow = 0; blockRowArrow < 4; blockRowArrow++)
+        int temporaryBlock[5][5];
+        for (int blockRowArrow = 0; blockRowArrow < 5; blockRowArrow++)
         {
-            for (int blockColArrow = 0; blockColArrow < 4; blockColArrow++)
+            for (int blockColArrow = 0; blockColArrow < 5; blockColArrow++)
             {
-                temporaryBlock[blockColArrow][3 - blockRowArrow] = blk[blockRowArrow][blockColArrow];
+                temporaryBlock[blockColArrow][4 - blockRowArrow] = blk[blockRowArrow][blockColArrow];
             }
         }
 
-        for (int blockRowArrow = 0; blockRowArrow < 4; blockRowArrow++)
+        for (int blockRowArrow = 0; blockRowArrow < 5; blockRowArrow++)
         {
-            for (int blockColArrow = 0; blockColArrow < 4; blockColArrow++)
+            for (int blockColArrow = 0; blockColArrow < 5; blockColArrow++)
             {
                 blk[blockRowArrow][blockColArrow] = temporaryBlock[blockRowArrow][blockColArrow];
             }
@@ -113,19 +113,21 @@ public:
 
     void show()
     {
-        cout << blk[0][0] << " " << blk[0][1] << " " << blk[0][2] << " " << blk[0][3] << endl;
-        cout << blk[1][0] << " " << blk[1][1] << " " << blk[1][2] << " " << blk[1][3] << endl;
-        cout << blk[2][0] << " " << blk[2][1] << " " << blk[2][2] << " " << blk[2][3] << endl;
-        cout << blk[3][0] << " " << blk[3][1] << " " << blk[3][2] << " " << blk[3][3] << endl;
+        cout << blk[0][0] << " " << blk[0][1] << " " << blk[0][2] << " " << blk[0][3] << " " << blk[0][4] << endl;
+        cout << blk[1][0] << " " << blk[1][1] << " " << blk[1][2] << " " << blk[1][3] << " " << blk[1][4] << endl;
+        cout << blk[2][0] << " " << blk[2][1] << " " << blk[2][2] << " " << blk[2][3] << " " << blk[2][4] << endl;
+        cout << blk[3][0] << " " << blk[3][1] << " " << blk[3][2] << " " << blk[3][3] << " " << blk[3][4] << endl;
+        cout << blk[4][0] << " " << blk[4][1] << " " << blk[4][2] << " " << blk[4][3] << " " << blk[4][4] << endl;
     }
 
     __block__()
     {
-        blk = new int *[4];
-        blk[0] = new int[4];
-        blk[1] = new int[4];
-        blk[2] = new int[4];
-        blk[3] = new int[4];
+        blk = new int *[5];
+        blk[0] = new int[5];
+        blk[1] = new int[5];
+        blk[2] = new int[5];
+        blk[3] = new int[5];
+        blk[4] = new int[5];
 
         nullBlock();
 
@@ -133,3 +135,19 @@ public:
         y = 3;
     }
 };
+
+int main()
+{
+    __block__ peice;
+    peice.Z();
+    peice.show();
+    cout << endl;
+    ~peice;
+    peice.show();
+    cout << endl;
+    ~peice;
+    peice.show();
+    cout << endl;
+    ~peice;
+    peice.show();
+}
